ccf6184853e8351bb2e9135bf188c053fd8609b2
李军磊 <749101340@qq.com>
2022-08-18 17:40:25 +0800

    eslint 配置更新

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,8 @@ module.exports = {
     '@typescript-eslint'
   ],
   'rules': {
+    // '@typescript-eslint/no-explicit-any': 0 // 允许any类型出现,但是违背TypeScript的初衷
+
     'array-bracket-spacing': [2, 'never', {'singleValue': true}], // 数组头尾有空格
     'comma-dangle': 2, // 禁止结尾逗号
     'comma-style': 2, // 强制使用一致的逗号风格
@@ -34,7 +36,7 @@ module.exports = {
     'array-element-newline': [2, 'consistent', { 'multiline': true }], // 数组换行
     'prefer-destructuring': [2, {'object': true}], // 优先使用解构
     'vue/multi-word-component-names': 0, // 关闭eslint检查文件名是否为驼峰命名
-    'indent': [2, 2] // 统一缩进
-    // '@typescript-eslint/no-explicit-any': 0 // 允许any类型出现,但是违背TypeScript的初衷
+    'indent': [2, 2], // 统一缩进
+    'semi': [2, 'never']
   }
 }
