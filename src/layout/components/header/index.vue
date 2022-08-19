f6c229ee340cb553b2050607013e43eb2e809f74
李军磊 <749101340@qq.com>
2022-08-19 22:41:02 +0800

    update

--- src/layout/components/header/index.vue
+++ src/layout/components/header/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <p>头部123</p>
+  <div class="page-header">
+    <div class="page-header-crumbs">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
+        <el-breadcrumb-item>
+          <a href="/">promotion management</a>
+        </el-breadcrumb-item>
+        <el-breadcrumb-item>promotion list</el-breadcrumb-item>
+        <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+
+    <div class="page-header-base"></div>
+  </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style lang="scss" scoped>
-
 </style>
