ecabbaf9435dc5d19f463c2fbe1e671ba972375f
李军磊 <749101340@qq.com>
2022-08-31 19:40:20 +0800

    页面加载进度条

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,17 @@ import App from './App.vue'
 import 'element-plus/dist/index.css'
 import { routers } from '@/router'
 import { createPinia } from 'pinia'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
+routers.beforeEach((to, from, next) => {
+  NProgress.start()
+  next()
+})
+
+routers.afterEach(() => {
+  NProgress.done()
+})
 
 const app = createApp(App)
 
